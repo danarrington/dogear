f57f9689650f82e975c52a22206534126c645c4c
Dan Arrington <dan.arrington@gmail.com>
2014-01-25 14:35:55 -0800

    Refactored factory usage in validation tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,16 +2,15 @@ require 'spec_helper'
 
 describe Book do
   it 'is valid with title, pages and current_page' do
-    book = Book.new(title: 'A book', pages: 105, current_page: 0)
-    expect(book).to be_valid
+    expect(build(:book)).to be_valid
   end
 
   it 'is invalid without a title' do
-    expect(Book.new(title: nil)).to have(1).errors_on(:title)
+    expect(build(:book, title: nil)).to have(1).errors_on(:title)
   end
 
   it 'is invalid without pages' do
-    expect(Book.new(pages: nil, title: 'book title')).to have(1).errors_on(:pages)
+    expect(build(:book, pages: nil, title: 'book title')).to have(1).errors_on(:pages)
   end
 
 
