606c638e4804576cda985fbf9856796d37c66178
Dan Arrington <dan.arrington@gmail.com>
2014-02-22 09:56:50 -0800

    Cleaned up auth links

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,6 +9,11 @@ describe UsersController do
       expect(User.first.email).to eq email
     end
 
+    it 'sets the name of the new user' do
+      name = Faker::Name.first_name
+      post :create, user: attributes_for(:user, name: name)
+      expect(User.first.name).to eq name
+    end
     it 'signs in the new user' do
       post :create, user: attributes_for(:user)
       expect(session[:user_id]).to_not be_nil
