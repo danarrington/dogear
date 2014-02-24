630f172b54f2684312f879654d6edc0926900e6a
Dan Arrington <dan.arrington@gmail.com>
2014-02-23 22:11:42 -0800

    Styling

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
    end
 
    def index
-      @books = Book.where(user: current_user)
+      @books = Book.where(user: current_user).order(updated_at: :desc)
    end
 
    def new
