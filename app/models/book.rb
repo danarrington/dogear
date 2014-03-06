d0bd77111c8b72dbfd5b02489cdf9c254111348c
Dan Arrington <dan.arrington@gmail.com>
2014-03-05 22:02:25 -0800

    Fixed utc time bug with book stats

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,15 +8,15 @@ class Book < ActiveRecord::Base
   validates :user, presence: true
 
   def pace
-    days = (Date.today - started_at.to_date).to_f
+    days = (Time.now.utc.to_date - started_at.to_date).to_f
     (current_page/days).round(1)
   end
 
   def finish_date
-    started_at.to_date + (pages.to_f/current_page*(Date.today-started_at.to_date)).to_i if current_page > 0
+    started_at.to_date + (pages.to_f/current_page*(Time.now.utc.to_date-started_at.to_date)).to_i if current_page > 0
   end
 
   def finish_date_days
-    (finish_date - Date.today).to_i if finish_date
+    (finish_date - Time.now.utc.to_date).round if finish_date
   end
 end
