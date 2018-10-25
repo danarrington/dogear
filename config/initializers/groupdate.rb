1a11c43cc6b85ad2301d726350eec25b6bb96550
Dan Arrington <dan.arrington@gmail.com>
2018-10-25 06:08:28 -0700

    Disable groupdate timezones for tests

--- config/initializers/groupdate.rb
+++ config/initializers/groupdate.rb
@@ -1,1 +1,4 @@
-Groupdate.time_zone = "Pacific Time (US & Canada)"
+unless Rails.env.test?
+  # Timezones not supported for sqlite (our test db)
+  Groupdate.time_zone = "Pacific Time (US & Canada)"
+end
