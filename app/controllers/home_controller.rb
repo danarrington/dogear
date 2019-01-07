13d18c0e1024b255d608717a9889b01879d6c4eb
Dan Arrington <dan.arrington@gmail.com>
2019-01-07 06:13:06 -0800

    Better home.index handling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,13 @@
 
 class HomeController < ApplicationController
   def index
-    if current_user
-      current_book = Book.where(user: current_user).order(updated_at: :desc).first
-      redirect_to current_book if current_book
-      redirect_to books_path
+    return render :index unless current_user
+
+    current_book = Book.where(user: current_user).order(updated_at: :desc).first
+    if current_book
+      redirect_to current_book
     else
-      render :index
+      redirect_to books_path
     end
   end
 end
